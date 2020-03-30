51acc0cfdabbe93761ad25e7cf6256fabae57860
paulasilvestrem <paulasivestre19@gmail.com>
2020-03-30 01:59:04 -0300

    teste
    teste

---
+++ script.js
@@ -0,0 +1,25 @@
+var a = 2;
+var b = 3;
+var c = "float";
+
+var soma = a+b;
+var sub  = a-b;
+var mult = a*b;
+var div  = a/b;
+/*
+alert(soma);
+alert(sub);
+alert(mult);
+alert(div);
+*/
+
+if (c%a == 1){
+    alert("Número ímpar");
+}
+else if (c%2 == 0){
+    alert("número par");
+}
+else {
+        alert("Valor inválido");
+    }
+    
